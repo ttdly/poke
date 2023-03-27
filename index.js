fdb1c59af5b535d03744bd3246717d04cb798ecd
ttdlyu <ttdlyu@163.com>
2023-03-27 20:45:30 +0800

    feat:discussions同步

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const getDiscussion = require('./scripts/discussions')
 
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const type = core.getInput('type');
+    const path = core.getInput('path');
+    const token = core.getInput('token');
+    const number = parseInt(core.getInput('number'));
+    const repository = core.getInput('repository');
+    if (type === 'discussion') {
+      getDiscussion(token, number, path, repository)
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
+
+run();
+
+// async function run() {
+//   try {
+//     core.debug('???')
+//     const type = core.getInput('type');
+//     core.debug(String(type));
+//     const path = core.getInput('path');
+//     const token = core.getInput('token');
+//     const number = core.getInput('number');
+//     const repository = core.getInput('repository');
+//     core.debug(String(type));
+//     if (type === 'discussion') {
+//       await getDiscussion(token,number,path,repository)
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     core.setFailed(error.message);
+//   }
+// }
 
 run();
